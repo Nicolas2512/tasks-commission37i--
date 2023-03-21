0de419cf6fbd54feb03c9222b6d863f1b7a1a357
Nicolas Moreno <franconicolas2512@gmail.com>
2023-03-21 12:35:32 -0300

    Agregue nueva carpeta con ejercicios resueltos

---
+++ js/02-Diagrama-de-flujo/flowrchat.js
@@ -0,0 +1,42 @@
+//! EJERCICIO 1
+// let lado, promedio;
+// lado = parseInt(prompt('Ingrese un lado del cuadrado'));
+// promedio = lado * 4;
+// document.write(`El perimetro del cuadrado es ${promedio}`);
+
+//! EJERCICIO 2
+// let numero1, numero2, numero3, numero4, suma, producto;
+// numero1 = parseInt(prompt('Ingrese su primer número'));
+// numero2 = parseInt(prompt('Ingrese su segundo número'));
+// numero3 = parseInt(prompt('Ingrese su tercer número'));
+// numero4 = parseInt(prompt('Ingrese su cuarto número'));
+
+// suma = console.log(`La suma del 1º y 2º número es de: ${numero1 + numero2}`);
+// producto = console.log(`El producto del 3º y 4º número es de: ${numero3 * numero4}`);
+
+//! EJERCICIO 3
+// let numero1, numero2, numero3, numero4, suma, producto;
+// numero1 = parseInt(prompt('Ingrese su primer número'));
+// numero2 = parseInt(prompt('Ingrese su segundo número'));
+// numero3 = parseInt(prompt('Ingrese su tercer número'));
+// numero4 = parseInt(prompt('Ingrese su cuarto número'));
+
+// suma = console.log(`La suma de los números cargados es de: ${numero1 + numero2 + numero3 + numero4}`);
+// producto = console.log(`El producto de los números cargados es de: ${numero1 * numero2 * numero3 * numero4}`);
+
+//! EJERCICIO 4
+// let precioArticulo, cantidad, totalAbonar;
+// precioArticulo = parseInt(prompt('Ingrese el precio del producto seleccionado'));
+// cantidad = parseInt(prompt('Indique la cantidad del producto seleccionado'));
+// totalAbonar = console.log(`El cliente debe abonar un total de: $${precioArticulo * cantidad}`);
+
+//! EJERCICIO 5
+// let nota1, nota2, nota3, suma, promedio;
+// nota1 = parseInt(prompt('Ingrese la primera nota del alumno'));
+// nota2 = parseInt(prompt('Ingrese la primera nota del alumno'));
+// nota3 = parseInt(prompt('Ingrese la primera nota del alumno'));
+// suma = nota1 + nota2 + nota3;
+// promedio = suma / 3;
+// document.write(`El promedio del alumno es de: ${promedio}`);
+
+//! EJERCICIO 6
--- js/exercise4.js
+++
@@ -1,6 +0,0 @@
-//! Ejercicio 4
-
-let precioArticulo, cantidad, totalAbonar;
-precioArticulo = parseInt(prompt('Ingrese el precio del producto seleccionado'));
-cantidad = parseInt(prompt('Indique la cantidad del producto seleccionado'));
-totalAbonar = console.log(`El cliente debe abonar un total de: $${precioArticulo * cantidad}`);
