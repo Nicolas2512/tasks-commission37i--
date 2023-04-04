45e0a1aa6b4a4ce13ca63c26aa98bb925df879f9
Nicolas Moreno <franconicolas2512@gmail.com>
2023-04-04 11:06:31 -0300

    resolvi ejercicio del TP1

---
+++ js/03-TP1-Primeros-ejercicios-de-js/solution-1.js
@@ -0,0 +1,72 @@
+//? EJERCICIO 1
+// alert('Un mensaje');
+
+//?EJERCICIO 2
+// document.write('Hello World');
+
+//?EJERCICIO 3
+// document.write(3+5);
+
+//?EJERCICIO 4
+// let nombre;
+// nombre = prompt('Ingrese su nombre');
+// document.write(`Hola  ${nombre}`);
+
+//?EJERCICIO 5
+// let numero1, numero2;
+// numero1 = parseInt(prompt(`Ingrese su primer número`));
+// numero2 = parseInt(prompt(`Ingrese su segundo número`));
+// alert(`La suma de los números ingresados es de: ${numero1 + numero2}`);
+
+//?EJERCICIO 6
+// let numero1, numero2;
+// numero1 = parseInt(prompt(`Ingrese su primer número`));
+// numero2 = parseInt(prompt(`Ingrese su segundo número`));
+// alert(`El mayor número de los ingresados es el: ${Math.max(numero1,numero2)}`);
+
+//?EJERCICIO 7
+// let numero1, numero2, numero3;
+// numero1 = parseInt(prompt(`Ingrese su primer número`));
+// numero2 = parseInt(prompt(`Ingrese su segundo número`));
+// numero3 = parseInt(prompt(`Ingrese su tercer número`));
+// if(numero1 > numero2 && numero1 > numero3){
+//     alert(`El mayor número ingresado es el: ${numero1}`);
+// }else if(numero2> numero1 && numero2 > numero3){
+//     alert(`El mayor número ingresado es el: ${numero2}`);
+// }else{
+//     alert(`El mayor número ingresado es el: ${numero3}`);
+// }
+
+//?EJERCICIO 8
+// let numero;
+// numero = parseInt(prompt(`Ingresa un número`));
+// if(numero % 2 === 0){
+//     alert(`El número: ${numero}, es divisible por 2`);
+// }else{
+//     alert(`El número: ${numero}, es no divisible por 2`);
+// }
+
+//?EJERCICIO 9
+// let frase, vocales= "";
+// frase = prompt("Ingrese una frase:");
+// for (let i = 0; i < frase.length; i++) {
+// let letra = frase.charAt(i).toLowerCase();
+// if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+//     vocales += letra;
+// }
+// }
+// alert(`Las vocales de la frase son: ${vocales}`);
+
+//?EJERCICIO 10
+// let numero;
+// numero = parseInt(prompt(`Ingrese un número`));
+// if(numero % 2 === 0){
+//     alert(`El ${numero} es divisible por 2.`)
+// }else if(numero % 3 === 0){
+//     alert(`El ${numero} es divisible por 3.`)
+// }else if(numero % 5 === 0){
+//     alert(`El ${numero} es divisible por 5.`)
+// }else if(numero % 7 === 0){
+//     alert(`El ${numero} es divisible por 7.`)
+// }
+
